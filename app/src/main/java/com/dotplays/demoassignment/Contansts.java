782b78a98ea7bff77cc96771cade42d7b6b7f7e5
HuyNguyen <huuhuybn@gmail.com>
2019-10-07 08:07:58 +0700

    Initial commit

---
+++ app/src/main/java/com/dotplays/demoassignment/Contansts.java
@@ -0,0 +1,8 @@
+package com.dotplays.demoassignment;
+
+public class Contansts {
+
+    public static final String API_KEY = "e83279923e8701adb493a9b3a9c413ca";
+    public static final String USER_ID = "184057905@N03";
+
+}
---
+++ app/src/main/java/com/dotplays/demoassignment/MainActivity.java
@@ -0,0 +1,55 @@
+package com.dotplays.demoassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadPhotos();
+
+    }
+
+
+    public void loadPhotos() {
+
+        AndroidNetworking.post("https://www.flickr.com/services/rest/")
+                .addBodyParameter("method", "flickr.favorites.getList")
+                .addBodyParameter("api_key", "24bf810575bc5bfbe2aef1ed6cd4517b")
+                .addBodyParameter("user_id", "184057905@N03")
+                .addBodyParameter("format", "json")
+                .addBodyParameter("nojsoncallback", "1")
+                .addBodyParameter("extras", "views,media,path_alias,url_sq,url_t,url_s,url_q,url_m,url_n,url_z,url_c,url_l,url_o")
+                .addBodyParameter("per_page", "10")
+                .addBodyParameter("page", "1")
+                .setTag("test")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // do anything with response
+                        Toast.makeText(MainActivity.this, response.toString(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                    }
+                });
+    }
+}
